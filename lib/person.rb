100c3cf9c8b8aeae25ee32e7b9e88408d098ffa7
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-27 22:54:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,17 @@
 class Person
  
- def initialize(name:, birthday:, height:, weight:)
+ def initialize(name:, birthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:)
    @name = name
    @birthday = birthday
    @height = height
    @weight = weight
+   @handed = handded
+   @complexion = complexion
+   @t_shirt_size = t_shirt_size
+   @wrist_size = wrist_size
+   @glove_size = glove_size
+   @pant_length = pant_length
+   @pant_width = pant_width
  end
  
 end
